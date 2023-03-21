9e30083bf2013686a33c5c2a0b45e3d85f73218f
akishtp <makishtp@gmail.com>
2023-03-21 23:23:47 +0530

    let's work on the backend for sometime now

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -5,22 +5,15 @@ import "./Home.css";
 const Home: React.FC = () => {
   return (
     <div className="home">
-      <Blog
-        title="I write blogs now Loll 7"
-        date="innale"
-        body="biggest blog yet by the akish written as a joke to make a body filled with textlore ipsum with a very lareg text is writtern is which is larger than the previous two text also making this the alrgets blog that has been written by me in this fake b log website yet here which may of bediffernet sizes and made to look as if it was a very fine day in the suburbs of us and maerica of the 90th centiury"
-      />
-      <Blog
-        title="I write blogs now Loll"
-        date="innale"
-        body="kore valiya body"
-      />
-      <Blog
-        title="I write blogs now Loll 2"
-        date="innale"
-        body="lore ipsum with a very lareg text is writtern here which may of bediffernet sizes and made to look as if it was a very fine day in the suburbs of us and maerica of the 90th centiury"
-      />
-      <Blog
+      <div className="new-blogs">
+        <div className="blog1"></div>
+        <div className="blog-2n3">
+          <div className="blog2"></div>
+          <div className="blog3"></div>
+        </div>
+      </div>
+      <div className="older-blogs"></div>
+      {/* <Blog
         title="I write blogs now Loll 3"
         date="innale"
         body="lore ipsum with a very lareg text is writtern is which is larger than the previous two text also making this the alrgets blog that has been written by me in this fake b log website yet here which may of bediffernet sizes and made to look as if it was a very fine day in the suburbs of us and maerica of the 90th centiury"
@@ -36,7 +29,7 @@ const Home: React.FC = () => {
         title="I write blogs now Loll 7"
         date="innale"
         body="biggest blog yet by the akish written as a joke to make a body filled with textlore ipsum with a very lareg text is writtern is which is larger than the previous two text also making this the alrgets blog that has been written by me in this fake b log website yet here which may of bediffernet sizes and made to look as if it was a very fine day in the suburbs of us and maerica of the 90th centiury"
-      />
+      /> */}
     </div>
   );
 };
