ae8c9476254ada105e7f6d048cafe451b1c0f294
akishtp <makishtp@gmail.com>
2023-12-10 00:32:17 +0530

    🗑️: trash posts

--- src/app/[id]/edit/page.tsx
+++ src/app/[id]/edit/page.tsx
@@ -2,16 +2,60 @@
 
 import { ThoughtProps } from "@/app/types";
 import Inputs from "@/components/Inputs";
+import { TrashIcon } from "@heroicons/react/24/outline";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { FC } from "react";
 import { SubmitHandler } from "react-hook-form";
 
-export default function Edit() {
+interface EditProps {
+  params: {
+    id: string;
+  };
+}
+
+const Edit: FC<EditProps> = ({ params }) => {
+  const router = useRouter();
+
   const handleEditPost: SubmitHandler<ThoughtProps> = (data) => {
     console.log(data);
   };
+
+  const { mutate: handleDelete, isPending } = useMutation({
+    mutationFn: async () => {
+      return axios.delete(`/api/thoughts/${params.id}`);
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+    onSuccess: () => {
+      router.push("/");
+      router.refresh();
+    },
+  });
+
   return (
     <div>
-      <div className="text-4xl">Edit...</div>
+      <div className="flex justify-between">
+        <div className="text-4xl">Edit...</div>
+        <button
+          className="bg-red-500 flex text-white items-center px-3 min-w-10 justify-center"
+          onClick={() => handleDelete()}
+        >
+          {isPending ? (
+            "Please wait"
+          ) : (
+            <>
+              <TrashIcon className="w-5" />
+              Delete
+            </>
+          )}
+        </button>
+      </div>
       <Inputs submit={handleEditPost} isEditing={true} />
     </div>
   );
-}
+};
+
+export default Edit;
---
+++ src/app/api/thoughts/[id]/route.ts
@@ -0,0 +1,40 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+interface contextProps {
+  params: {
+    id: string;
+  };
+}
+
+export async function DELETE(req: Request, context: contextProps) {
+  try {
+    await prisma.thought.delete({
+      where: {
+        id: context.params.id,
+      },
+    });
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
+
+export async function PATCH(req: Request, context: contextProps) {
+  try {
+    const body = await req.json();
+    await prisma.thought.update({
+      where: {
+        id: context.params.id,
+      },
+      data: {
+        title: body.title,
+        content: body.content,
+        img: body.img,
+      },
+    });
+    return NextResponse.json({ message: "update success" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
