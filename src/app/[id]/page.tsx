02d74fce3fc8686c7ebbe8e0b9b34f5ca4faeb3c
akishtp <makishtp@gmail.com>
2023-12-09 19:58:40 +0530

    🧐: view each thought deeply

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,40 +1,32 @@
-const Blog = () => {
+import prisma from "@/lib/prisma";
+import { FC } from "react";
+
+interface BlogProps {
+  params: {
+    id: string;
+  };
+}
+
+async function getThought(id: string) {
+  const response = await prisma.thought.findFirst({
+    where: { id: id },
+    select: { id: true, img: true, title: true, content: true },
+  });
+  return response;
+}
+
+const Blog: FC<BlogProps> = async ({ params }) => {
+  const thought = await getThought(params.id);
   return (
     <div>
-      <div className="text-4xl">Title</div>
+      <div className="text-4xl">{thought?.title}</div>
       <div>
-        <div className="float-right w-1/2 pr-12 pl-8 pb-10">
-          <img
-            src="https://images.unsplash.com/photo-1596428693280-648ba296233c?q=80&w=2944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            className="cool-shadow-two"
-          />
-        </div>
-        <p className="text-justify pt-6">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique
-          sint perferendis atque consectetur rem vero architecto ipsa delectus,
-          voluptas ut, ea maiores doloremque voluptatem voluptates nisi
-          laboriosam, aperiam ex ducimus? Lorem ipsum dolor sit, amet
-          consectetur adipisicing elit. Libero aliquid consectetur aspernatur!
-          Consectetur, sapiente unde voluptates, sint magni repudiandae animi
-          id, modi quam voluptate veritatis necessitatibus dignissimos velit ad
-          in! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Voluptatibus labore rerum exercitationem atque non eius? Cum minima
-          vitae, impedit fugiat voluptatum recusandae eum nulla, ipsum ut id
-          cupiditate sequi veniam? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Vero et error, cum libero assumenda numquam ratione
-          eligendi nulla explicabo molestias dolor. Aut quam, aliquid placeat
-          sint odio officiis eveniet architecto. Lorem, ipsum dolor sit amet
-          consectetur adipisicing elit. Reprehenderit provident mollitia
-          consequatur illum facilis maiores, expedita laudantium, quia quae
-          architecto incidunt inventore atque cumque ad placeat in a dignissimos
-          amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-          cum quo nostrum libero possimus asperiores ea eos repudiandae atque
-          sapiente veniam temporibus, officia eius reiciendis assumenda! Hic
-          blanditiis nesciunt sapiente. Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Blanditiis esse rem perspiciatis nulla commodi
-          veniam! Nesciunt voluptatum saepe tempora quam sint magnam sunt! Ipsum
-          iure hic ipsa, non consectetur quia.
-        </p>
+        {thought?.img && (
+          <div className="float-right w-1/2 pr-12 pl-8 pb-10">
+            <img src={thought?.img} className="cool-shadow-two" />
+          </div>
+        )}
+        <p className="text-justify pt-6">{thought?.content}</p>
       </div>
     </div>
   );
