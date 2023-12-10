075703156330c287bb6c399d46d16f8cf21eaa58
akishtp <makishtp@gmail.com>
2023-12-10 19:23:42 +0530

    ⚠️: prisma client error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,13 +4,8 @@ declare global {
   var prisma: PrismaClient;
 }
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+prisma = new PrismaClient();
+global.prisma = new PrismaClient();
+prisma = global.prisma;
 
 export default prisma;
